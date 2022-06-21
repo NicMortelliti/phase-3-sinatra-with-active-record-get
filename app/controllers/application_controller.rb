baba8631d0054e26f6fea64939dacfd38c82395d
NicMortelliti <npmortelliti@gmail.com>
2022-06-20 18:21:14 -0700

    Return json formatted games list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < Sinatra::Base
+   # Add this line to set the Content-Type header for all responses
+  set :default_content_type, 'application/json'
 
   get '/' do
     { message: "Hello world" }.to_json
+  end
+
+  get '/games' do
+    # get all the games from the database
+    games = Game.all.order(:title).limit(10)
+
+    # return a JSON response with an array of all the game data
+    games.to_json
   end
 
 end
